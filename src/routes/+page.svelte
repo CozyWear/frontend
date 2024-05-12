1bc7538b3b002c82c0b041b26482140cead2a3f2
Harishankar G <harishankargaddanakeri@gmail.com>
2024-05-12 22:34:06 +0530

    Move components into its seperate folder, move aboutus page to its route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,55 @@
-<script src="https://cdn.tailwindcss.com"></script> 
-<body class="bg-[#f2ece1]">
-  <main class="max-w-4xl mx-auto p-8 bg-[#dbebf5]">
-    <nav class="mb-8 flex justify-between items-center ">
-      <div>
-        <a href="/" class="mr-4 inline-block py-2 px-4 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md">Home</a>|
-        <a href="about" class="mr-4 inline-block py-2 px-4 font-bold rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md">About Us</a>|
-        <a href="login" class="mr-4 inline-block py-2 px-4 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md">Login</a>|
-        <a href="register" class="inline-block py-2 px-4 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md">Register</a>
-      </div>
-    </nav>
-    <header class="py-4">
-      <h1 class="text-3xl font-bold text-center">About Us</h1>
-    </header>
-    <section class="mb-8">
-      <h2 class="text-xl font-semibold mb-4 text-center">Welcome to CozyWear!</h2>
-      <p class="mb-2 text-justify">Welcome to our world of bespoke elegance, where the artistry of our merchants and tailors intertwines to create a symphony of style and substance. Here, the journey of sartorial splendor begins with the selection of luxurious fabrics from our esteemed merchants, each imbued with a story waiting to be told. With a palette of possibilities in hand, our customers embark on a voyage of self-expression, exploring the exquisite designs and intricate details offered by our masterful tailors. As they curate their unique ensemble, our innovative preview feature brings their vision to life, allowing them to step into the realm of their creation and see themselves adorned in perfection. With every stitch, we weave together craftsmanship and creativity, ensuring that each garment is a reflection of the wearer's distinct personality and refined taste. Moreover, rest assured, our priority is to ensure your attire is not just stylish but also incredibly comfy, crafted with the finest materials for ultimate comfort. Welcome to a realm where dreams are tailored into reality, and every shopping experience is a masterpiece in the making.</p>
-    </section>
-    <footer class="text-gray-600 text-sm flex justify-center">
-      <p>&copy; 2024 CozyWear. All rights reserved.</p>
-    </footer>
-  </main>
-</body>
-  
+<script lang="ts">
+	import Modal from '$lib/components/Modal.svelte';
+	let showModal = false;
+	const ToggleModal = () => {
+		showModal = !showModal;
+	};
+</script>
+
+<Modal {showModal} on:click={ToggleModal}>
+	<h3>Contact Us</h3>
+
+	<form>
+		<input type="text" placeholder="Name" class="inputs" />
+		<br />
+		<input type="text" placeholder="Email" class="inputs" />
+		<br />
+		<select
+			style="width: 80%; margin:5% auto; border:1px solid ; padding:12px 8px; border-radius:10px; background-color: #fefefe;"
+		>
+			<option value="Customer">Customer</option>
+			<option value="Merchant">Merchant</option>
+			<option value="Tailor">Tailor</option>
+		</select>
+		<br />
+		<textarea name="Complaint" style="width: 80%; margin:5%;" placeholder="Enter Your Feedback Here"
+		></textarea>
+		<br />
+		<button>Submit</button>
+	</form>
+</Modal>
+
+<div class="button-container">
+	<button on:click={ToggleModal} class="buttons">Contact Us</button>
+</div>
+
+<style>
+	.inputs {
+		background-color: #fefefe;
+		margin: 5% auto;
+		padding: 10px;
+		border: 1px solid #888;
+		width: 80%;
+		border-radius: 10px;
+	}
+	button {
+		border: 0;
+		cursor: pointer;
+		padding: 8px 12px;
+		font-weight: bold;
+		box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+	}
+	h3 {
+		font-size: 24px;
+	}
+</style>
---
+++ src/routes/aboutus/+page.svelte
@@ -0,0 +1,52 @@
+<body class="bg-[#f2ece1]">
+	<main class="max-w-4xl mx-auto p-8 bg-[#dbebf5]">
+		<nav class="mb-8 flex justify-between items-center">
+			<div>
+				<a
+					href="/"
+					class="mr-4 inline-block py-2 px-4 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md"
+					>Home</a
+				>|
+				<a
+					href="aboutus"
+					class="mr-4 inline-block py-2 px-4 font-bold rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md"
+					>About Us</a
+				>|
+				<a
+					href="login"
+					class="mr-4 inline-block py-2 px-4 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md"
+					>Login</a
+				>|
+				<a
+					href="register"
+					class="inline-block py-2 px-4 rounded-lg shadow transition duration-300 ease-in-out hover:shadow-md"
+					>Register</a
+				>
+			</div>
+		</nav>
+		<header class="py-4">
+			<h1 class="text-3xl font-bold text-center">About Us</h1>
+		</header>
+		<section class="mb-8">
+			<h2 class="text-xl font-semibold mb-4 text-center">Welcome to CozyWear!</h2>
+			<p class="mb-2 text-justify">
+				Welcome to our world of bespoke elegance, where the artistry of our merchants and tailors
+				intertwines to create a symphony of style and substance. Here, the journey of sartorial
+				splendor begins with the selection of luxurious fabrics from our esteemed merchants, each
+				imbued with a story waiting to be told. With a palette of possibilities in hand, our
+				customers embark on a voyage of self-expression, exploring the exquisite designs and
+				intricate details offered by our masterful tailors. As they curate their unique ensemble,
+				our innovative preview feature brings their vision to life, allowing them to step into the
+				realm of their creation and see themselves adorned in perfection. With every stitch, we
+				weave together craftsmanship and creativity, ensuring that each garment is a reflection of
+				the wearer's distinct personality and refined taste. Moreover, rest assured, our priority is
+				to ensure your attire is not just stylish but also incredibly comfy, crafted with the finest
+				materials for ultimate comfort. Welcome to a realm where dreams are tailored into reality,
+				and every shopping experience is a masterpiece in the making.
+			</p>
+		</section>
+		<footer class="text-gray-600 text-sm flex justify-center">
+			<p>&copy; 2024 CozyWear. All rights reserved.</p>
+		</footer>
+	</main>
+</body>
