d8b8c7942fafd1afd4de5517f82e2dcc4fd0a31b
Prerana-G <117432274+Prerana-G@users.noreply.github.com>
2024-05-16 13:57:43 +0530

    added show/hide password icon
    added show/hide password icon

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { api_url } from '$lib/constants';
 	import { customer_id, tailor_id, merchant_id } from '$lib/constants';
+	import { onMount } from 'svelte';
 
 	let usertype: string,
 		postalcode: number,
@@ -20,6 +21,7 @@
 		streetAddressInputError: string,
 		cityInputError: string,
 		postalCodeInputError: string;
+	let showPassword: boolean = false;
 
 	function validateFirstName() {
 		const nameRegex = /^[A-Za-z]+$/;
@@ -167,6 +169,13 @@
 
 		return true;
 	}
+	function togglePasswordVisibility() {
+        	showPassword = !showPassword;
+    	}
+
+    	onMount(() => {
+        	togglePasswordVisibility();
+    	});
 </script>
 
 <svelte:head>
@@ -246,14 +255,38 @@
 		{/if}
 
 		<label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password:</label>
-		<input
-			class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
-			type="password"
-			id="password"
-			bind:value={password}
-			on:input={validatePassword}
-			required
-		/>
+		<div class="relative">			
+			{#if showPassword}
+                		<input
+                    			class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline pr-10"
+                    			id="password"
+                    			type="password"
+                    			bind:value={password}
+		    			on:input={validatePassword}
+                    			required
+                		/>
+            		{:else}
+                		<input
+                    			class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline pr-10"
+                    			id="password"
+                    			type="text"
+                    			bind:value={password}
+		    			on:input={validatePassword}
+                    			required
+               			/>
+            		{/if}
+			<div class="absolute top-1.5 right-2">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" stroke-width="1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" on:click={togglePasswordVisibility} aria-label="Toggle password visibility">
+					{#if showPassword}
+						<path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+						<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+
+					{:else}
+						<path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />	
+					{/if}
+				</svg>
+			</div>		  
+		</div>
 		{#if passwordInputError}
 			<p class="text-red-500 text-xs italic">{passwordInputError}</p>
 		{/if}
@@ -347,14 +380,15 @@
 				<option value="West Bengal">West Bengal</option>
 			</select>
 			<div
-				class="pointer-events-none absolute inset-y-0 right-0 flex justify-center items-center px-2 text-gray-700"
-			>
-				<svg class="fill-current h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-					<path
-						fill-rule="evenodd"
-						d="M5.293 7.293a1 1 0 0 1 1.414 0L10 10.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 0-1.414z"
-					/>
-				</svg>
+				class="pointer-events-none absolute inset-y-0 right-0 flex justify-center items-center px-2 text-gray-700">
+				<div class="absolute top-1.5 right-2">
+					<svg class="fill-current h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+						<path
+							fill-rule="evenodd"
+							d="M5.293 7.293a1 1 0 0 1 1.414 0L10 10.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 0-1.414z"
+						/>
+					</svg>
+				</div>
 			</div>
 		</div>
 		<!-- <input
