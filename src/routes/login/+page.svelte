796fafe203c5531e9bbbd8733e62c349978edcba
Harishankar G <harishankargaddanakeri@gmail.com>
2024-05-19 13:08:56 +0530

    Fix typo in login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -34,18 +34,18 @@
 			console.error('Error logging in:', error);
 		}
 	}
+
 	function togglePasswordVisibility() {
-        	showPassword = !showPassword;
-    	}
+		showPassword = !showPassword;
+	}
 
-
-    	onMount(() => {
-        	togglePasswordVisibility();
-    	});
+	onMount(() => {
+		togglePasswordVisibility();
+	});
 </script>
 
 <svelte:head>
-	<title>Register</title>
+	<title>Login</title>
 	<meta
 		name="description"
 		content="Login to CozyWear for access to your account. Enjoy personalized shopping experiences and manage your orders seamlessly. Sign in now!"
@@ -69,13 +69,13 @@
 		</div>
 		<div class="mb-4">
 			<label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password:</label>
-			<div class="relative">			
+			<div class="relative">
 				{#if showPassword}
 					<input
 						class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline pr-10"
 						id="password"
 						type="password"
-						bind:value={password}						
+						bind:value={password}
 						required
 					/>
 				{:else}
@@ -88,16 +88,36 @@
 					/>
 				{/if}
 				<div class="absolute top-1.5 right-2">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" stroke-width="1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" on:click={togglePasswordVisibility} aria-label="Toggle password visibility">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6"
+						stroke-width="1.5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						on:click={togglePasswordVisibility}
+						aria-label="Toggle password visibility"
+					>
 						{#if showPassword}
-							<path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-							<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-	
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+							/>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+							/>
 						{:else}
-							<path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />	
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+							/>
 						{/if}
 					</svg>
-				</div> 
+				</div>
 			</div>
 		</div>
 		<div class="mb-6">
