70f60205426601e7ba39e7a05d2556740a27b32d
h-aryna <haryan998@gmail.com>
2024-07-17 20:09:35 +0530

    made changes in popup and made customize

---
+++ src/routes/customize/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import NavBar from '$lib/components/NavBar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import Popup from '$lib/components/Popup.svelte';
+
+	let showPopup = false;
+	let imageSrc = '';
+
+	function handleImageSelected(event) {
+		imageSrc = event.detail.src;
+		showPopup = false;
+	}
+</script>
+
+<NavBar />
+<div class="Container">
+	<div class="img">
+		<img src={imageSrc} alt="Selected Image" />
+	</div>
+</div>
+<Footer />
+
+<style>
+</style>
